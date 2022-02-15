a7698407cf1525d843ac2ea9a737bdccc0160942
Artur <artwis1998@gmail.com>
2022-02-15 23:00:43 +0100

    Migrate android plugin v2 embedding

--- android/src/main/java/com/htdevelopers/pdfviewer/PdfViewerPlugin.java
+++ android/src/main/java/com/htdevelopers/pdfviewer/PdfViewerPlugin.java
@@ -3,19 +3,31 @@ package com.htdevelopers.pdfviewer;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+
 /**
  * PdfViewerPlugin
  */
-public class PdfViewerPlugin {
+public class PdfViewerPlugin implements FlutterPlugin {
     static MethodChannel channel;
 
-    /**
-     * Plugin registration.
-     */
+    @SuppressWarnings("deprecation")
     public static void registerWith(Registrar registrar) {
         registrar
-        .platformViewRegistry()
-        .registerViewFactory(
-                "pdf_platform_view/view", new PdfViewFactory(registrar.messenger()));
+                .platformViewRegistry()
+                .registerViewFactory(
+                        "pdf_platform_view/view", new PdfViewFactory(registrar.messenger()));
     }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        flutterPluginBinding
+                .getPlatformViewRegistry()
+                .registerViewFactory(
+                        "pdf_platform_view/view", new PdfViewFactory(flutterPluginBinding.getBinaryMessenger()));
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {}
 }
